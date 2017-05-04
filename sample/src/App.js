797a074433909ecc7e331c6dc193cb66d2e54b25
Matteo Taroli <taroli.matteo@gmail.com>
2017-05-04 22:02:48 +0200

    Fix sample lint

--- sample/src/App.js
+++ sample/src/App.js
@@ -4,34 +4,29 @@ import ContextMenu from 'react-contextmenu-component';
 
 class App extends Component {
     getOptions() {
-      return [
-        [
-          {
-            label: 'Configure',
-            onClick: () => console.log('Configure')
-          },
-          {
-            label: 'Disabled',
-            onClick: () => console.log('Disabled'),
-            disabled: true
-          }
-        ],
-        [
-          {
-            label: 'Delete',
-            onClick: () => console.log('delete')
-          }
-        ]
-      ];
+        return [
+            [
+                {
+                    label: 'Configure',
+                    onClick: () => console.log('Configure')
+                }, {
+                    label: 'Disabled',
+                    onClick: () => console.log('Disabled'),
+                    disabled: true
+                }
+            ],
+            [
+                {
+                    label: 'Delete',
+                    onClick: () => console.log('delete')
+                }
+            ]
+        ];
     }
     render() {
         return (
             <div>
-                <ContextMenu
-                    options={this.getOptions()}
-                    id="context-menu-container"
-                    className="context-menu-container"
-                  >
+                <ContextMenu options={this.getOptions()} id="context-menu-container" className="context-menu-container">
                     <p>Right-click anywhere on me to show the context menu ;)</p>
                 </ContextMenu>
             </div>
