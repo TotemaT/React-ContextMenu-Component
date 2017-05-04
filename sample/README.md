b2f6a3b4dabfbf676be657aaca9638a1dc1fb629
Matteo Taroli <taroli.matteo@gmail.com>
2017-05-04 21:26:53 +0200

    Add sample

---
+++ sample/README.md
@@ -0,0 +1,11 @@
+# React-contextmenu-component Example
+
+Simple example for react-contextmenu-component ([github](https://github.com/TotemaT/React-ContextMenu-Component) | [npm](https://www.npmjs.com/package/react-contextmenu-component)), made using [react-create-app](https://github.com/facebookincubator/create-react-app).
+
+## Try it
+Run the app with the following commands
+```
+  git clone https://github.com/TotemaT/React-ContextMenu-Component-Example
+  cd React-ContextMenu-Component-Example
+  npm start
+```
---
+++ sample/src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import './App.css';
+import ContextMenu from 'react-contextmenu-component';
+
+class App extends Component {
+    getOptions() {
+      return [
+        [
+          {
+            label: 'Configure',
+            onClick: () => console.log('Configure')
+          },
+          {
+            label: 'Disabled',
+            onClick: () => console.log('Disabled'),
+            disabled: true
+          }
+        ],
+        [
+          {
+            label: 'Delete',
+            onClick: () => console.log('delete')
+          }
+        ]
+      ];
+    }
+    render() {
+        return (
+            <div>
+                <ContextMenu
+                    options={this.getOptions()}
+                    id="context-menu-container"
+                    className="context-menu-container"
+                  >
+                    <p>Right-click anywhere on me to show the context menu ;)</p>
+                </ContextMenu>
+            </div>
+        );
+    }
+}
+
+export default App;
